5cda62b95ff172abcb4f76fe477f5a8b2bb31405
kmeKame <68630209+kmeKame@users.noreply.github.com>
2023-08-18 11:56:09 -0300

    Exercício: Antecessor e sucessor!

---
+++ desafio001/resultado.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 001</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Resultado:</h1>
+    </header>
+    <main>
+
+        <?php
+        $result = $_REQUEST["valor"];
+        $antecessor = $result - 1;
+        $sucessor = $result + 1;
+        echo "O antecessor de $result é <strong>$antecessor</strong>";
+        echo "<br> O sucessor de $result é <strong>$sucessor</strong>";
+        ?>
+        <button onclick="javascript:history.go(-1)"> VOLTAR </button>
+    </main>
+
+</body>
+
+</html>
